165f3f9acb4338b41e3c5a3ba0cc67e2515cac41
James Wyatt <78618659+Arctic-Gif@users.noreply.github.com>
2021-09-19 01:50:38 -0700

    Table and image url

--- 01-back-end/src/main/java/com/jwcdeveloper/ecommerceshop/dao/ProductCategoryRepository.java
+++ 01-back-end/src/main/java/com/jwcdeveloper/ecommerceshop/dao/ProductCategoryRepository.java
@@ -3,7 +3,9 @@ package com.jwcdeveloper.ecommerceshop.dao;
 import com.jwcdeveloper.ecommerceshop.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "ProductCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
